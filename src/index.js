bafe1d4fa65130339e386c295bdb8bcf19d0c4ab
Chris <ChrisNosowsky@gmail.com>
2020-08-05 00:12:48 -0400

    nodejs application setup

--- src/index.js
+++ src/index.js
@@ -1,7 +1,12 @@
-express = require('express')
+const express = require('express')
+const path = require('path')
+
 const app = express()
+const port = process.env.PORT || 3000
+const publicDirectoryPath = path.join(__dirname, '../public')
 
-app.use(express.json)
+app.use(express.static(publicDirectoryPath))
 
-
-
+app.listen(port, () => {
+    console.log(`App is up on port ${port}!`)
+})
