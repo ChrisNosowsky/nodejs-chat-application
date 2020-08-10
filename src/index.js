6a7b140a333d4c4bb32bd375e2320e9383cb1a67
Chris <ChrisNosowsky@gmail.com>
2020-08-09 22:22:24 -0400

    socketio example with incrementing count from server to all clients

--- src/index.js
+++ src/index.js
@@ -12,9 +12,22 @@ const publicDirectoryPath = path.join(__dirname, '../public')
 
 app.use(express.static(publicDirectoryPath))
 
-io.on('connection', () => {
+let count = 0
+
+// server (emit) -> client (receive) - countUpdated
+// client (emit) -> server (receive) - increment
+
+
+io.on('connection', (socket) => {
     console.log('New Websocket Connection')
+
+    socket.emit('countUpdated', count)
+    socket.on('increment', () => {
+        count++
+        io.emit('countUpdated', count)
+    })
 })
+
 
 server.listen(port, () => {
     console.log(`App is up on port ${port}!`)
