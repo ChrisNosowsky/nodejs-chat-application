3721f8104759c821d99b4d6ab6ddf4a99e6e4d9c
Chris <ChrisNosowsky@gmail.com>
2020-08-10 21:41:23 -0400

    checking for profanity

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ const express = require('express')
 const http = require('http')
 const path = require('path')
 const socketio = require('socket.io')
+const Filter = require('bad-words')
 
 const app = express()
 const server = http.createServer(app)
