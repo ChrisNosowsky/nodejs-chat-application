c8d389d66c2d053f5faf3099a98f8a2153f2348d
Chris <ChrisNosowsky@gmail.com>
2020-08-12 21:42:56 -0400

    adding moment time formatting to chat messages

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ const http = require('http')
 const path = require('path')
 const socketio = require('socket.io')
 const Filter = require('bad-words')
+const {generateMessage} = require('./utils/messages')
 
 const app = express()
 const server = http.createServer(app)
@@ -16,21 +17,21 @@ app.use(express.static(publicDirectoryPath))
 io.on('connection', (socket) => {
     console.log('New Websocket Connection')
 
-    socket.emit('message', "Welcome!")
+    socket.emit('message', generateMessage('Welcome!'))
 
-    socket.broadcast.emit('message', 'A new user has joined')
+    socket.broadcast.emit('message', generateMessage('A new user has joined'))
 
     socket.on('sendMessage', (msg, callback) => {
         const filter = new Filter()
         if (filter.isProfane(msg)) {
             return callback('Profanity is not allowed!')
         }
-        io.emit('message', msg)
+        io.emit('message', generateMessage(msg))
         callback()
     })
 
     socket.on('disconnect', () => {
-        io.emit('message', 'A user has left!')
+        io.emit('message', generateMessage('A user has left!'))
     })
 
     socket.on('sendLocation', (position, callback) => {
