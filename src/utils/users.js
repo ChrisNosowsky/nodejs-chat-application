c81808f38cbbe46b3cf39f299b2f0263bd1711aa
Chris <ChrisNosowsky@gmail.com>
2020-08-13 22:38:03 -0400

    finishing the functions for storing users

--- src/utils/users.js
+++ src/utils/users.js
@@ -1,34 +1,25 @@
 const users = []
 
-
-// addUser, removeUser, getUser, getUsersInRoom
-
-
 const addUser = ({ id, username, room}) => {
-    // clean the data
     username = username.trim().toLowerCase()
     room = room.trim().toLowerCase()
 
-    // validate the data
     if (!username || !room) {
         return {
             error: 'Username and room are required'
         }
     }
 
-    // check for existing user
     const existingUser = users.find((user) => {
         return user.room === room && user.username === username
     })
 
-    // validate username
     if (existingUser) {
         return {
             error: 'Username is in use'
         }
     }
 
-    // store user
     const user = {id, username, room}
     users.push(user)
     return {user}
@@ -42,17 +33,19 @@ const removeUser = (id) => {
     }
 }
 
-addUser({
-    id: 20,
-    username: 'Chris',
-    room: 'Detroit'
-})
 
+const getUser = (id) => {
+    return users.find((user) => user.id === id)
+}
 
-console.log(users)
+const getUsersInRoom = (room) => {
+    room = room.trim().toLowerCase()
+    return users.filter((user) => user.room === room)
+}
 
-const removedUser = removeUser(20)
-
-console.log(removedUser)
-
-console.log(users)
+module.exports = {
+    addUser,
+    removeUser,
+    getUser,
+    getUsersInRoom
+}
