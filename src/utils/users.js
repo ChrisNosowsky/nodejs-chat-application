31810da6fe7c1bd0125991fbefc7fb7fef839c92
Chris <ChrisNosowsky@gmail.com>
2020-08-13 22:22:05 -0400

    storing users in room

---
+++ src/utils/users.js
@@ -0,0 +1,58 @@
+const users = []
+
+
+// addUser, removeUser, getUser, getUsersInRoom
+
+
+const addUser = ({ id, username, room}) => {
+    // clean the data
+    username = username.trim().toLowerCase()
+    room = room.trim().toLowerCase()
+
+    // validate the data
+    if (!username || !room) {
+        return {
+            error: 'Username and room are required'
+        }
+    }
+
+    // check for existing user
+    const existingUser = users.find((user) => {
+        return user.room === room && user.username === username
+    })
+
+    // validate username
+    if (existingUser) {
+        return {
+            error: 'Username is in use'
+        }
+    }
+
+    // store user
+    const user = {id, username, room}
+    users.push(user)
+    return {user}
+}
+
+const removeUser = (id) => {
+    const index = users.findIndex((user) => user.id === id)
+
+    if (index !== -1) {
+        return users.splice(index, 1)[0]
+    }
+}
+
+addUser({
+    id: 20,
+    username: 'Chris',
+    room: 'Detroit'
+})
+
+
+console.log(users)
+
+const removedUser = removeUser(20)
+
+console.log(removedUser)
+
+console.log(users)
